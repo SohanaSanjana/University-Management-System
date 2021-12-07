532364880aeedc0f070d9fa31cb33076ae0bfd62
SohanaSanjana <95612790+SohanaSanjana@users.noreply.github.com>
2021-12-07 21:26:52 +0600

    Add files via upload

---
+++ Model/AllocateClassRoom.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UniversityMvcApp.Models
+{
+    public class AllocateClassRoom
+    {
+        public int Id { get; set; }
+
+        public int DepartmentId { get; set; }
+        public Department Department { get; set; }
+
+        public int CourseId { get; set; }
+        public Course Course { get; set; }
+
+        public int RoomId { get; set; }
+        public Room Room { get; set; }
+
+        public string Day { get; set; }
+
+        public string From { get; set; }
+
+        public string TO { get; set; }
+        public TimeSpan StartTime { get; set; }
+
+        public TimeSpan FinishTime { get; set; }
+
+    }
+}
---
+++ Model/Department.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace UniversityMvcApp.Models
+{
+    public class Department
+    {
+        public int ID { get; set; }
+
+
+        [StringLength(7, MinimumLength = 2)]
+        [Required(ErrorMessage = "Code entered must be between 2 to 7 characters long")]
+        public string Code { get; set; }
+
+
+       [Required(ErrorMessage = "Please enter a department name")]
+        public string Name { get; set; }
+
+       public virtual List<Course> Courses { get; set; }
+       public virtual List<Student> Students { get; set; } 
+       public virtual List<Teacher> Teachers { get; set; }
+       public virtual List<AllocateClassRoom> AllocateClassRooms { get; set; }
+       public List<Room> Rooms { get; set; }
+       
+    }
+}
